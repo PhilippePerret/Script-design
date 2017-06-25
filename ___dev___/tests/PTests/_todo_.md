111428001d935a6f34a3c1e4c1e9b9d74295bb09
Philippe Perret <philippe.perret@yahoo.fr>
2017-06-25 18:53:10 +0200

    Poursuite test des tests

--- ___dev___/tests/PTests/_todo_.md
+++ ___dev___/tests/PTests/_todo_.md
@@ -8,3 +8,9 @@
 * Options de méthode de comparaison (2e argument). Pouvoir mettre `not_a_test: true` pour dire que l'évaluation ne doit pas produire d'expectative. C'est utiliser lorsqu'on fait le test de l'application, pour étudier le retour. Voir ci-dessous
   * => tester (comment ?)
   * => documenter (dans le refbook du programmeur seulement ?)
+* Documenter le test `instanceOf(...)` en précisant que l'argument peut être le nom de la classe en string (mais elle doit être connue du renderer process)
+* Implémenter `with()` qui est utilisé après `call` pour injecter des arguments
+  * => tester
+  * => documenter
+* Pouvoir envoyer un array `[Object, methode]` qui sera utilisé avec `call`. Donc on fera `Objet[methode].call()`
+* Documenter et tester l'option `no_values: true` qui dit au test de ne pas écrire la vraie valeur quand un actual_str ou un expect_str est utilisé. Par exemple, si on a `expect(112, "l'âge du capitaine")`, le message commencera par "l'âge du capitaine (112)". Si `no_values: true` est utilisé (attention : dans le 3e argument de la méthode de comparaison, en fin de chaine), alors seulement "l'âge du capitaine" sera inscrit.
--- tests/ptests/autotests/template_message_spec.js
+++ tests/ptests/autotests/template_message_spec.js
@@ -5,51 +5,91 @@
 */
 log('-> template_message_spec.js')
 
-describe("Essai de récupération du message écrit")
-  .it('retourne seulement l’objet expect', () => {
-    let fn = function(){return expect(4).equals(4)}
-    expect(fn.call()).to.be.instanceof('PTestExpectObject')
-  })
-describe('Valeur humaine pour actual')
-  .it('est utilisé dans le message de succès', () => {
-    expect(42, "l'âge du capitaine").to.be.equal_to(42)
-  })
-  .and('et dans le message d’échec [FAILURE VOLONTAIRE]', () => {
-    expect(42, "l'âge du capitaine").to.be.equal_to(40)
-  })
+let res
 
-describe('Valeur humaine pour la valeur attendue')
-  .it('est utilisée dans le message de succès', () => {
-    expect(42).to.equals(42, "l'âge du capitaine")
-  })
-  .and('et dans le message d’échec [FAILURE VOLONTAIRE]', () => {
-    expect(41).to.equals(42, "l'âge du capitaine")
-  })
+describe('Contrôle général des messages de rapport du test')
+  .describe("Messages en cas de succès")
+    .context('sans message template')
+      .it('écrit le message normal', () => {
+        res = expect(4).equals(4, {not_a_test:true}).returnedMessage
+        expect(res, 'le retour de "equal"').contains('4 est égal à 4', {no_values: true})
+      })
+    .context('avec des « valeurs pseudo » pour actual et expect')
+      .context('et la vraie valeur ajoutée')
+        .it('utilise la valeur pseudo pour actual', () => {
+          res = expect(24, 'l’âge de Marion').equals(24, {not_a_test:true})
+          expect(res.returnedMessage).strictly.contains("l’âge de Marion (24) est égal à 24")
+        })
+//     .it('utilise la valeur pseudo pour expected', () => {
+//       res = expect(24).equals(24, 'l’âge de Marion', {not_a_test:true})
+//       expect(res.returnedMessage).strictly.contains("24 est égal à l’âge de Marion (24)")
+//     })
+// .context('avec un message template complet')
+//   .it('utilise le template', () => {
+//     res = expect(24).equals(24, {template:{success:"l'âge de Marion est __EXPECTED__ ans."}, not_a_test:true})
+//     expect(res.returnedMessage).contains("l'âge de Marion est 24 ans.")
+//   })
+//
+// describe("Messages en cas d'échec")
+// .context('sans message template')
+//   .it('écrit le message par défaut', () => {
+//     let res = expect(5).equals(4, {not_a_test:true})
+//     expect(res.isOK).equals(false)
+//     expect(res.returnedMessage).contains('5 n’est pas égal à 4')
+//   })
+// .context('avec des « valeurs pseudo » pour actual et expect')
 
-describe('Des valeurs humaine pour actual et expected peuvent être')
-  .it("utilisé pour le message de succès", () => {
-    let
-          age_capitaine = 42
-        , age_femme     = 42
-    expect(age_capitaine, "l'âge du capitaine").to.be.equal_to(age_femme, "l'âge de sa femme")
-  })
-  .and("et le message d'échec [FAILURE VOLONTAIRE]", () => {
-    let
-          age_capitaine = 42
-        , age_femme     = 41
-    expect(age_capitaine, "l'âge du capitaine").to.be.equal_to(age_femme, "l'âge de sa femme")
-  })
 
-describe("On peut utiliser un message personnalisé")
-  .it('pour le message de succès', () => {
-    expect(4).to.equal(4, null, {template:{success:"les deux valeurs sont bien égales"}})
-  })
-  .it('pour le message d’échec [FAILURE VOLONTAIRE]', () => {
-    expect(4).to.equal(5, null, {template:{failure:"les deux valeurs divergent trop"}})
-  })
-  .it('pour le message d’échec (traitement comme test) [FAILURE VOLONTAIRE]', () => {
-    let oExpect = function(){return expect(4).to.equal(5, null, {template:{failure:"les deux valeurs divergent trop"}})}
-    expect(oExpect.returnedMessage).to.contains("les deux valeurs divergent trop")
-  })
 
-log('<- template_message_spec.js')
+
+
+
+
+
+
+
+
+
+// describe('Valeur humaine pour actual')
+//   .it('est utilisé dans le message de succès', () => {
+//     expect(42, "l'âge du capitaine").to.be.equal_to(42)
+//   })
+//   .and('et dans le message d’échec [FAILURE VOLONTAIRE]', () => {
+//     expect(42, "l'âge du capitaine").to.be.equal_to(40)
+//   })
+//
+// describe('Valeur humaine pour la valeur attendue')
+//   .it('est utilisée dans le message de succès', () => {
+//     expect(42).to.equals(42, "l'âge du capitaine")
+//   })
+//   .and('et dans le message d’échec [FAILURE VOLONTAIRE]', () => {
+//     expect(41).to.equals(42, "l'âge du capitaine")
+//   })
+//
+// describe('Des valeurs humaine pour actual et expected peuvent être')
+//   .it("utilisé pour le message de succès", () => {
+//     let
+//           age_capitaine = 42
+//         , age_femme     = 42
+//     expect(age_capitaine, "l'âge du capitaine").to.be.equal_to(age_femme, "l'âge de sa femme")
+//   })
+//   .and("et le message d'échec [FAILURE VOLONTAIRE]", () => {
+//     let
+//           age_capitaine = 42
+//         , age_femme     = 41
+//     expect(age_capitaine, "l'âge du capitaine").to.be.equal_to(age_femme, "l'âge de sa femme", {no_values: true})
+//   })
+//
+// describe("On peut utiliser un message personnalisé")
+//   .it('pour le message de succès', () => {
+//     expect(4).to.equal(4, null, {template:{success:"les deux valeurs sont bien égales"}})
+//   })
+//   .it('pour le message d’échec [FAILURE VOLONTAIRE]', () => {
+//     expect(4).to.equal(5, null, {template:{failure:"les deux valeurs divergent trop"}})
+//   })
+//   .it('pour le message d’échec (traitement comme test) [FAILURE VOLONTAIRE]', () => {
+//     let oExpect = function(){return expect(4).to.equal(5, null, {template:{failure:"les deux valeurs divergent trop"}})}
+//     expect(oExpect.returnedMessage).to.contains("les deux valeurs divergent trop")
+//   })
+//
+// log('<- template_message_spec.js')
