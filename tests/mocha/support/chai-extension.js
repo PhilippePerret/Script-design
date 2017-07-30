f1826fbff2bdc2bfc92ad13a1803d357196ac631
Philippe Perret <philippe.perret@yahoo.fr>
2017-07-30 11:52:13 +0200

    Traitement de `not.to.haveTag` (mocha-chai)

--- tests/mocha/support/chai-extension.js
+++ tests/mocha/support/chai-extension.js
@@ -25,40 +25,102 @@ const plugin = function plugin(chai, utils) {
     }
     return true
   })
-  Assertion.addMethod('haveTag', function assertion(tag, attrs){
-    const self = this
-    // console.log('this = ', this)
-    var obj = utils.flag(self, 'object') // => HTMLElement
+
+  /**
+  * Prend une balise (+tag+) et des attributs (+attrs+) et
+  * @return {String} le code pour le message de retour.
+  *
+  * La complication vient qu'on peut passer dans +attrs+ des propriétés qui
+  * ne sont pas des attributs HTML, comme par exemple `count` ou `text`
+  **/
+  function buildMessageTag( tag, attrs )
+  {
+    let attrsStr = '' ; nombre_fois = 'a tag ' ; texte = ''
+    delete attrs.tag
+    if ( attrs ){
+      let s = attrs.count > 1 ? 's' : ''
+      if ( attrs.count ) {nombre_fois = `${attrs.count} tag${s} ` ; delete attrs.count}
+      if ( attrs.text  ) {texte = `${attrs.text}</${tag}>` ; delete attrs.text }
+      if ( attrs )
+      {
+        attrsStr = []
+        for(let a in attrs){if (attrs.hasOwnProperty(a)){attrsStr.push(`${a}="${attrs[a]}"`)}}
+        attrsStr = ' ' + attrsStr.join(' ')
+      }
+    }
+    return `${nombre_fois}<${tag}${attrsStr}>${texte}`
+  }
+
+  /**
+  * Définition de l'assertion pour 'haveTag'
+  **/
+  function assertHaveTag ( tag, attrs, msg )
+  {
+    if (msg) { utils.flag(this, 'message', msg) }
+    const obj = utils.flag(this, 'object')
+
+    // On teste
     if ( ! obj ) { throw new Error('Actual is null.') }
+    attrs || ( attrs = {} )
     attrs.tag = tag
     let resultat = DOMTest.actualTagContainsExpect(obj, attrs)
-    if ( resultat.success )
-    {
-      return true
-    }
-    else
-    {
-      // En cas d'erreur on construit le texte
-      var asSource = obj.outerHTML.replace(/[\n\r]/g,'')
-      if (asSource.length > 80){
-        asSource = asSource.substr(0,60) + '...' + asSource.substr(-20, 20)
-      }
-      let attrsStr = '' ; nombre_fois = '' ; texte = ''
-      delete attrs.tag
-      if ( attrs ){
-        if ( attrs.count ) {nombre_fois = `${attrs.count}: ` ; delete attrs.count}
-        if ( attrs.text  ) {texte = `${attrs.text}</${tag}>` ; delete attrs.text }
-        if ( attrs )
-        {
-          attrsStr = []
-          for(let a in attrs){if (attrs.hasOwnProperty(a)){attrsStr.push(`${a}="${attrs[a]}"`)}}
-          attrsStr = ' ' + attrsStr.join(' ')
-        }
-      }
-      var expAsStr = `${nombre_fois}<${tag}${attrsStr}>${texte}`
-      resultat.error = String(resultat.error).replace(/_ACTUAL_/g, expAsStr)
-      throw new Error(resultat.error)
-    }
-  })
+    let exp = buildMessageTag(tag, attrs)
+    this.assert(
+        resultat.success
+      , `expected #{this} to have ${exp}`
+      , `expected #{this} to not have ${exp}`
+      , tag
+      , this._obj
+      , true
+    )
+
+    // // Modèle tiré de l'assert assertEqual
+    // this.assert(
+    //     val === obj
+    //   , 'expected #{this} to equal #{exp}'
+    //   , 'expected #{this} to not equal #{exp}'
+    //   , val
+    //   , this._obj
+    //   , true
+    // );
+  }
+
+  Assertion.addMethod('haveTag', assertHaveTag)
+
+  // Assertion.addMethod('haveTag', function assertion(tag, attrs){
+  //   const self = this
+  //   // console.log('this = ', this)
+  //   var obj = utils.flag(self, 'object') // => HTMLElement
+  //   if ( ! obj ) { throw new Error('Actual is null.') }
+  //   attrs.tag = tag
+  //   let resultat = DOMTest.actualTagContainsExpect(obj, attrs)
+  //   if ( resultat.success )
+  //   {
+  //     return true
+  //   }
+  //   else
+  //   {
+  //     // En cas d'erreur on construit le texte
+  //     var asSource = obj.outerHTML.replace(/[\n\r]/g,'')
+  //     if (asSource.length > 80){
+  //       asSource = asSource.substr(0,60) + '...' + asSource.substr(-20, 20)
+  //     }
+  //     let attrsStr = '' ; nombre_fois = '' ; texte = ''
+  //     delete attrs.tag
+  //     if ( attrs ){
+  //       if ( attrs.count ) {nombre_fois = `${attrs.count}: ` ; delete attrs.count}
+  //       if ( attrs.text  ) {texte = `${attrs.text}</${tag}>` ; delete attrs.text }
+  //       if ( attrs )
+  //       {
+  //         attrsStr = []
+  //         for(let a in attrs){if (attrs.hasOwnProperty(a)){attrsStr.push(`${a}="${attrs[a]}"`)}}
+  //         attrsStr = ' ' + attrsStr.join(' ')
+  //       }
+  //     }
+  //     var expAsStr = `${nombre_fois}<${tag}${attrsStr}>${texte}`
+  //     resultat.error = String(resultat.error).replace(/_ACTUAL_/g, expAsStr)
+  //     throw new Error(resultat.error)
+  //   }
+  // })
 }
 module.exports = plugin
